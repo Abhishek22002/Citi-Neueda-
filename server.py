1a4d73fec2025da89b4a3cf50cfd8405b3636d4d
Abhishek22002 <41200654+Abhishek22002@users.noreply.github.com>
2022-06-29 13:54:02 -0500

    Changed html to inline again

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
             
 @app.route("/")
 def newHome():
-    return render_template("game.html")
+    return '<html><body><div>EEEEEEEEEEEEEEEEE</div></body></html>'
 
 
 # Start the applictaion.
